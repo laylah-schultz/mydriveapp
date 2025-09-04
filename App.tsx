0b21720b8728f3698876b3ba10c0e20a2ba87250
laylah-schultz <ST10467386@vcconnect.edu.za>
2025-09-04 14:51:20 +0200

    added heading, changed styles etc

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, SafeAreaView, Text, View, Image} from 'react-native';
+
 
 export default function App() {
   const age = 15; 
@@ -19,12 +20,28 @@ export default function App() {
            }
   
   
+          
   return (
-    <View style={styles.container}>
+
+    <SafeAreaView style={styles.container}>  
+    <View style = {styles.content}>
+
+    <Text style={[styles.response, {fontSize: 48}]}> Driving License Checker </Text>
+
+      <Image source = {{uri:'https://media.istockphoto.com/id/1407324683/photo/traffic-control-on-street.jpg?b=1&s=612x612&w=0&k=20&c=WO1uXv9Om5xODRJBEs87mj5lt4OaqzWHdLYtHv1x40M=',}} 
+      style = {styles.image}/>
       
+
+      <Text style={styles.response}>Age: {age} </Text>
+      <Text style={styles.response}>License Years: {licenseyears} </Text>
       <Text style={styles.response}> {feedback} </Text>
       
+
+      
+
     </View>
+   </SafeAreaView>
+    
   ); 
 }
 
@@ -35,10 +52,24 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  
+  content: {
+    width: '100%',
+    alignItems: 'center'
+  },
 
   response: {
     fontSize: 28,
     color: 'white',
+    fontWeight: 'bold',
+    marginVertical: 30
 
+  },
+
+  image: {
+    width: '100%',
+    height: '50%', 
+    resizeMode: 'contain',
+    top:0,
   }
 });
